91342d17e939fce70adf1fc7e1749050f84a35d0
divyapolson <divyapolson@gmail.com>
2018-02-24 19:51:43 -0600

    made tradeback slider responsive + added pagination

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,44 @@
 //lazy load
-[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-  img.setAttribute('src', img.getAttribute('data-src'));
-  img.onload = function() {
-    img.removeAttribute('data-src');
-  };
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
 });
 
 //carousel sliders
 var swiper1 = new Swiper('.swiper1', {
-      slidesPerView: 3,
-      spaceBetween: 40,
-      pagination: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    pagination: {
         el: '.swiper-pagination1',
         clickable: true,
-      },
-      navigation: {
+    },
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
+    },
+    breakpoints: {
         1340: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         906: {
-          slidesPerView: 1,
-          spaceBetween: 20,
+            slidesPerView: 1,
+            spaceBetween: 20,
         },
-      }
+    }
 });
 
 var swiper2 = new Swiper('.swiper2', {
-      slidesPerView: 1,
-      pagination: {
+    slidesPerView: 1,
+    calculateHeight: true,
+    pagination: {
         el: '.swiper-pagination2',
         clickable: true,
-      },
-      navigation: {
+    },
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
+    },
 });
